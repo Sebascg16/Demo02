ad24a4c1b35ae99e6244b0ad7917f2adc50286d8
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2024-12-05 12:18:49 -0500

    Creación Clase Employee y Abstracciones

---
+++ MyLibrary/Employee.cs
@@ -0,0 +1,36 @@
+﻿namespace MyLibrary;
+
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int GetInt()
+        {
+            return 5;
+        }
+        public virtual decimal CalculateMonthPayment()
+        {
+            return 12000;
+        }
+              
+    }
+
+public class Secretary : Employee
+{
+    public override decimal CalculateMonthPayment() => 15000;
+    
+}
+
+public class Manager : Employee
+{
+    public override decimal CalculateMonthPayment()
+    {
+        return 30000;
+    }
+}
+
+public class Developer : Employee
+{
+
+}
